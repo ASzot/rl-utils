d35363e9fdb44ed8a01c5584dd32d28099eaa05a
ASzot <me@andrewszot.com>
2022-05-21 22:48:28 -0700

    Updated GitHub jobs

--- rl_helper/envs/vec_env/__init__.py
+++ rl_helper/envs/vec_env/__init__.py
@@ -1,9 +1,14 @@
 from .dummy_vec_env import DummyVecEnv
 from .shmem_vec_env import ShmemVecEnv
 from .subproc_vec_env import SubprocVecEnv
-from .vec_env import (AlreadySteppingError, CloudpickleWrapper,
-                      NotSteppingError, VecEnv, VecEnvObservationWrapper,
-                      VecEnvWrapper)
+from .vec_env import (
+    AlreadySteppingError,
+    CloudpickleWrapper,
+    NotSteppingError,
+    VecEnv,
+    VecEnvObservationWrapper,
+    VecEnvWrapper,
+)
 from .vec_frame_stack import VecFrameStack
 from .vec_monitor import VecMonitor
 
